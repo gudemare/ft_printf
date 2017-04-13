b0907bdd82543ab40b2ed6deab856445786b6d78
gudemare <gudemare@student.42.fr>
2017-04-13 19:22:01 +0200

    Updated ft_strextend

--- libft/srcs/string/ft_strextend.c
+++ libft/srcs/string/ft_strextend.c
@@ -6,7 +6,7 @@
 /*   By: gudemare <gudemare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/13 17:03:05 by gudemare          #+#    #+#             */
-/*   Updated: 2017/04/13 17:07:06 by gudemare         ###   ########.fr       */
+/*   Updated: 2017/04/13 17:14:02 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strextend(char *str, int extension)
 {
 	char	*new;
 
-	if (!(new = (char *)malloc(ft_strlen(str) + extension)))
+	if (!(new = ft_strnew(ft_strlen(str) + extension)))
 		return (0);
 	ft_strcpy(new, str);
 	free(str);
