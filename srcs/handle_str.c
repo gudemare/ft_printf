273ae2f4909dcb4910af3a546ff628a424828981
gudemare <gudemare@student.42.fr>
2017-05-10 23:40:14 +0200

    Precision handling on sS flags

--- srcs/handle_str.c
+++ srcs/handle_str.c
@@ -6,7 +6,7 @@
 /*   By: gudemare <gudemare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/09 14:35:39 by gudemare          #+#    #+#             */
-/*   Updated: 2017/04/10 02:34:20 by gudemare         ###   ########.fr       */
+/*   Updated: 2017/05/10 23:39:10 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ char	*handle_str(va_list ap, t_spec spec)
 {
 	char	*f_o;
 
-	if (!(f_o = ft_strdup(va_arg(ap, char *))))
+	if (!(f_o = (spec.precision == -1) ? ft_strdup(va_arg(ap, char *))
+				: ft_strndup(va_arg(ap, char *), spec.precision)))
 		return (0);
 	(void)spec;
 	return (f_o);
