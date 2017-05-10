af90c4b1ffb4a19afe64b4ddd662a32e79a44d53
gudemare <gudemare@student.42.fr>
2017-05-10 23:17:07 +0200

    Harsher test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gudemare <gudemare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/04 14:31:31 by gudemare          #+#    #+#             */
-/*   Updated: 2017/04/19 22:04:06 by gudemare         ###   ########.fr       */
+/*   Updated: 2017/05/10 23:14:36 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,11 @@ int		main(void)
 	int		ret;
 
 	printf("--==[Testing ft_printf]==--\nprintf :\n");
-	ret = printf("Bonjour %5d %12s %3c %#0- +10.3hh%!\n", 42, "Xavier", 'A');
+	ret = printf("Bonjour %.2d | %12s %3c %#0- +10.3hh%!\n"
+			, 42, "Xavier", 'A');
 	printf("(%d)\nft_printf :\n", ret);
-	ret = ft_printf("Bonjour %5d %12s %3c %#0- +10.3hh%!\n", 42, "Xavier", 'A');
+	ret = ft_printf("Bonjour %.2d | %12s %3c %#0- +10.3hh%!\n"
+			, 42, "Xavier", 'A');
 	printf("(%d)\n", ret);
 	return (0);
 }
