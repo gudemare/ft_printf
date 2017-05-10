b8f8010b5cc17e21603d75cf0eb0767ffb53bfc7
gudemare <gudemare@student.42.fr>
2017-05-10 23:48:31 +0200

    Starter type differentiation

--- srcs/handle_int.c
+++ srcs/handle_int.c
@@ -6,7 +6,7 @@
 /*   By: gudemare <gudemare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/09 14:35:41 by gudemare          #+#    #+#             */
-/*   Updated: 2017/04/10 02:35:51 by gudemare         ###   ########.fr       */
+/*   Updated: 2017/05/10 23:47:26 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ char	*handle_int(va_list ap, t_spec spec)
 {
 	char	*f_o;
 
-	if (!(f_o = ft_itoa(va_arg(ap, int))))
+	if (!(f_o = ft_itoa((spec.conv == 'u') ? (unsigned int)va_arg(ap, int)
+					: (int)va_arg(ap, int))))
 		return (0);
 	(void)spec;
 	return (f_o);
