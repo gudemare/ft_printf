0f81293fa30ac6aa605aafe236d4d335cfd5ba81
gudemare <gudemare@student.42.fr>
2017-05-11 19:55:56 +0200

    (null) %s handling

--- srcs/handle_str.c
+++ srcs/handle_str.c
@@ -6,7 +6,7 @@
 /*   By: gudemare <gudemare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/09 14:35:39 by gudemare          #+#    #+#             */
-/*   Updated: 2017/05/10 23:39:10 by gudemare         ###   ########.fr       */
+/*   Updated: 2017/05/11 19:53:22 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 char	*handle_str(va_list ap, t_spec spec)
 {
 	char	*f_o;
+	char	*str;
 
-	if (!(f_o = (spec.precision == -1) ? ft_strdup(va_arg(ap, char *))
-				: ft_strndup(va_arg(ap, char *), spec.precision)))
+	if (!(str = va_arg(ap, char*)))
+		return (ft_strdup("(null)"));
+	if (!(f_o = (spec.precision == -1) ? ft_strdup(str)
+				: ft_strndup(str, spec.precision)))
 		return (0);
 	(void)spec;
 	return (f_o);
