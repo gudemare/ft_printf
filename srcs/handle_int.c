3bb66ea83571bf2188d4ad1fe242095ecc1e8885
gudemare <gudemare@student.42.fr>
2017-05-11 18:40:05 +0200

    0 precision handling

--- srcs/handle_int.c
+++ srcs/handle_int.c
@@ -6,7 +6,7 @@
 /*   By: gudemare <gudemare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/09 14:35:41 by gudemare          #+#    #+#             */
-/*   Updated: 2017/05/10 23:47:26 by gudemare         ###   ########.fr       */
+/*   Updated: 2017/05/11 18:37:26 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@ char	*handle_int(va_list ap, t_spec spec)
 {
 	char	*f_o;
 
-	if (!(f_o = ft_itoa((spec.conv == 'u') ? (unsigned int)va_arg(ap, int)
-					: (int)va_arg(ap, int))))
+	if (!(f_o = ft_itoa((int)va_arg(ap, int))))
 		return (0);
+	if (spec.precision == 0 && ft_strcmp(f_o, "0") == 0)
+		*f_o = '\0';
 	(void)spec;
 	return (f_o);
 }
