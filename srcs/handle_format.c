4cf2f712290d7f936b01096c0bece4125eb97f9a
gudemare <gudemare@student.42.fr>
2017-05-16 16:02:24 +0200

    Fixed edge case with %0+Xd conversion

--- srcs/handle_format.c
+++ srcs/handle_format.c
@@ -6,14 +6,14 @@
 /*   By: gudemare <gudemare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/10 00:09:49 by gudemare          #+#    #+#             */
-/*   Updated: 2017/05/16 15:11:51 by gudemare         ###   ########.fr       */
+/*   Updated: 2017/05/16 15:59:05 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "ft_printf.h"
 
-static char		*min_width(char *f_o, int min_width, char *format)
+static char		*min_width(char *f_o, int min_width, char *format, char conv)
 {
 	char	*tmp;
 	char	c;
@@ -31,6 +31,14 @@ static char		*min_width(char *f_o, int min_width, char *format)
 			f_o, ft_strlen(f_o));
 	free(f_o);
 	f_o = tmp;
+	if (format && ft_strchr("di", conv) && ft_strchr(format, '+')
+			&& ft_strchr(format, '0'))
+	{
+		while (*tmp == '0')
+			tmp++;
+		*f_o = *tmp;
+		*tmp = '0';
+	}
 	return (f_o);
 }
 
@@ -39,7 +47,7 @@ int				handle_format(char *f_o, t_spec spec)
 	if ((size_t)spec.min_width > ft_strlen(f_o))
 		if (!(f_o = min_width(f_o,
 				spec.min_width - ((*f_o == '\0' && spec.conv == 'c') ? 1 : 0),
-				spec.format)))
+				spec.format, spec.conv)))
 			return (-1);
 	(void)spec;
 	ft_putstr(f_o);
